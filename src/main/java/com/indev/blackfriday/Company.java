3911604d4cd9a806ff14c89a9dbc186288d87fb0
Alaa Lakhdissi <alakhdissi@sqli.com>
2018-11-27 13:38:24 +0000

    Add private to list

--- src/main/java/com/indev/blackfriday/Company.java
+++ src/main/java/com/indev/blackfriday/Company.java
@@ -4,15 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Company {
-    Product p ;
-    List<Product> pList = new ArrayList<Product>();
+    private List<Product> pList = new ArrayList<Product>();
 
     public float sells(String capsule) {
         return 0;
     }
 
     public void stock(int i, String capsule, int i1) {
-        p = new Product(i,capsule,i1);
+        Product p = new Product(i,capsule,i1);
         pList.add(p);
     }
 
