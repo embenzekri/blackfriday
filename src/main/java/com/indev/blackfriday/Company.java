c25b59f717d4a29c7eef80b7a52796b1d64955a3
Abdelmounaim yamine <abdelmounaimyamine@MacBook-Pro-de-Abdelmounaim.local>
2018-11-27 12:21:14 -0800

    Tow first methode

--- src/main/java/com/indev/blackfriday/Company.java
+++ src/main/java/com/indev/blackfriday/Company.java
@@ -1,12 +1,36 @@
 package com.indev.blackfriday;
 
+import com.sun.deploy.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Company {
+
+    List<Product> productList = new ArrayList<Product>();
+
     public float sells(String capsule) {
-        return 0;
+        float sum = 0;
+        int total = 0;
+        for(int i = 0; i<productList.size(); i++) {
+
+            Product product = productList.get(i);
+            if(product.getName().equals(capsule)){
+                total =product.getPrice() * 5;
+                sum =  total + (total*20)/100 ;
+                product.setQuantity(product.getQuantity()-5);
+                break;
+            }
+        }
+        return sum;
     }
 
     public void stock(int i, String capsule, int i1) {
-
+        Product product = new Product();
+        product.setName(capsule);
+        product.setPrice(i1);
+        product.setQuantity(i);
+        productList.add(product);
     }
 
     public Company to(int i) {
@@ -18,7 +42,12 @@ public class Company {
     }
 
     public int totalAssets() {
-        return 0;
+        int total = 0;
+        for(int i = 0; i<productList.size(); i++){
+            Product product = productList.get(i);
+            total+= product.getPrice() * product.getQuantity();
+        }
+        return total;
     }
 
     public Company blackFriday() {
