c0225700c904f47a89ac5faded6ef8f5a6ab4d92
TOUKRICHTE Mohamed <mtoukrichte@sqli.com>
2018-11-27 11:45:12 +0000

    Change logic to use list of products instead of one product

--- src/main/java/com/indev/blackfriday/Company.java
+++ src/main/java/com/indev/blackfriday/Company.java
@@ -1,17 +1,19 @@
 package com.indev.blackfriday;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Company {
 
-    private Product product;
+    private List<Product> products = new ArrayList<Product>();
 
     public float sells(String capsule) {
         return 0;
     }
 
     public void stock(int i, String capsule, int i1) {
-        product = new Product(capsule, i1, i);
+        Product product = new Product(capsule, i1, i);
+        products.add(product);
     }
 
     public Company to(int i) {
@@ -23,7 +25,12 @@ public class Company {
     }
 
     public int totalAssets() {
-        return product.getPrice()*product.getQunatity();
+        int totalAssets = 0;
+        for (Product product: products
+             ) {
+            totalAssets += product.getPrice()*product.getQunatity();
+        }
+        return totalAssets;
     }
 
     public Company blackFriday() {
--- src/main/java/com/indev/blackfriday/Product.java
+++ src/main/java/com/indev/blackfriday/Product.java
@@ -1,4 +1,41 @@
 package com.indev.blackfriday;
 
 public class Product {
+
+    private String productName;
+    private int price;
+    private int qunatity;
+
+    public Product(String productName, int price, int qunatity) {
+        this.productName = productName;
+        this.price = price;
+        this.qunatity = qunatity;
+    }
+
+    public Product() {
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQunatity() {
+        return qunatity;
+    }
+
+    public void setQunatity(int qunatity) {
+        this.qunatity = qunatity;
+    }
 }
